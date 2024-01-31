737c7d6da64b2ae9020b7eb3dff9b2d31e0df574
fwefew123 <artem.andreev200012@mail.ru>
2024-01-31 12:02:23 +0600

    Version 1.0.3

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,24 +1,41 @@
 import React from "react";
+import { Link as ScrollLink } from "react-scroll";
 
 import "../Header/style.scss";
 
 import logo from "../../assets/logo.svg";
 import Hamburger from "hamburger-react";
-import Menu from "../Menu/Menu";
+
+import Button from "../Button/Button";
 
 const Header = (props) => {
   return (
-    <section className="header-mob">
-      <div className="header">
-        <img src={logo} alt="image" className="header__img" />
-        <Hamburger
-          toggled={props.isOpen}
-          toggle={props.toggleMenu}
-          color="#ed092d"
-          className="header__ham"
-        />
-      </div>
-    </section>
+    <>
+      <section className="header-mob">
+        <div className="header">
+          <img src={logo} alt="image" className="header__img" />
+          <Hamburger
+            toggled={props.isOpen}
+            toggle={props.toggleMenu}
+            color="#ed092d"
+            className="header__ham"
+          />
+        </div>
+      </section>
+      <section className="header-pc">
+        <div class="container">
+          <img src={logo} alt="image" class="container__logo" />
+          <nav className="container__links">
+            <ScrollLink class="container__links__item">Главная</ScrollLink>
+            <ScrollLink class="container__links__item">Бренды</ScrollLink>
+            <ScrollLink class="container__links__item">Контакты</ScrollLink>
+          </nav>
+          <ScrollLink class="container__item">
+            <button className="container__item__btn">Заказать звонок</button>
+          </ScrollLink>
+        </div>
+      </section>
+    </>
   );
 };
 
--- src/Screens/MainScreen/MainScreen.jsx
+++ src/Screens/MainScreen/MainScreen.jsx
@@ -8,6 +8,7 @@ import { Link as ScrollLink } from "react-scroll";
 
 import Car from '../../assets/image-3.webp'
 import Button from "../../Components/Button/Button";
+import CarPc from '../../assets/decoration.webp'
 
 const MainScreen = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -30,34 +31,62 @@ const MainScreen = () => {
     <>
       <Header isOpen={isOpen} toggleMenu={toggleMenu} />
       {isOpen && <Menu closeMenu={() => setIsOpen(false)} />}
-      <div className="main-all">
-        <TextMain
-          classNameC={'main'}
-          classNameP={'main__par'}
-          textFirst={'China Drive'}
-          classNameN={'main__nav'}
-          textBig={'Search, book, and order details easily'}
-          classNameS={'main__second'}
-          textSecond={'Lorem ipsum dolor sit amet consectetur. Sit porttitor vel magna vulputate.'} />
-        <ScrollLink smooth to="main__car">
-          <Button className={"main__btn"} text={'Подробнее'} />
-        </ScrollLink>
-      </div>
-
-      <div className="main-second">
-        <img src={Car} alt="car" className="main__car" />
-
-        <div className='main__s'>
-          <nav className='main__s__nav'>
-            Тут должен быть большой заголовок
-          </nav>
-          <p className='main__s__second'>
-            Lorem ipsum dolor sit amet consectetur. Etiam at leo egestas et amet tincidunt. Justo fringilla blandit nibh rhoncus.
-          </p>
-          <ScrollLink smooth to={'form'}>
-            <Button className={"main__s__btn"} text={'Контакты'} />
+      <section className="main-mob">
+        <div className="main-all">
+          <TextMain
+            classNameC={'main'}
+            classNameP={'main__par'}
+            textFirst={'China Drive'}
+            classNameN={'main__nav'}
+            textBig={'Search, book, and order details easily'}
+            classNameS={'main__second'}
+            textSecond={'Lorem ipsum dolor sit amet consectetur. Sit porttitor vel magna vulputate.'} />
+          <ScrollLink smooth to="main__car">
+            <Button className={"main__btn"} text={'Подробнее'} />
           </ScrollLink>
         </div>
+
+        <div className="main-second">
+          <img src={Car} alt="car" className="main__car" />
+
+          <div className='main__s'>
+            <nav className='main__s__nav'>
+              Тут должен быть большой заголовок
+            </nav>
+            <p className='main__s__second'>
+              Lorem ipsum dolor sit amet consectetur. Etiam at leo egestas et amet tincidunt. Justo fringilla blandit nibh rhoncus.
+            </p>
+            <ScrollLink smooth to={'form'}>
+              <Button className={"main__s__btn"} text={'Контакты'} />
+            </ScrollLink>
+          </div>
+        </div>
+      </section>
+      <section className="main-pc">
+        <div className="main_pc">
+          <p className="main_pc__first">China Drive</p>
+          <nav className="main_pc__big">Search, book, and order details easily</nav>
+          <nav className="main_pc__container">
+            <p className="main_pc__container__second">Lorem ipsum dolor sit amet consectetur. Sit porttitor vel magna vulputate.</p>
+            <hr className="main_pc__container__separator" />
+            <button className="main_pc__container__btn">Подробнее</button>
+          </nav>
+        </div>
+        <img src={CarPc} alt="car" className="main-pc__img" />
+      </section>
+      <div className="main_s">
+        <nav className="main_s__nav">
+          <nav className="main_s__big">
+            Тут должен быть большой заголовок
+          </nav>
+          <nav>
+            <hr className="main_s__separator" />
+          </nav>
+          <div className="main-s__items">
+            <p className="main_s__second">Lorem ipsum dolor sit amet consectetur. Etiam at leo egestas et amet tincidunt. Justo fringilla blandit nibh rhoncus. </p>
+            <button className="main_s__btn">Контакты</button>
+          </div>
+        </nav>
       </div>
     </>
   );
