ac07f0087d04cb1e939aa0e44154d45fe725acea
fwefew123 <artem.andreev200012@mail.ru>
2024-02-01 00:20:58 +0600

    Version 1.0.4

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,31 +1,85 @@
-import React from 'react'
+import React from "react";
 
-import '../Form/style.scss'
-import TextMain from '../TextMain/TextMain'
-import Button from '../Button/Button'
+import "../Form/style.scss";
+import TextMain from "../TextMain/TextMain";
+import Button from "../Button/Button";
 
 const Form = () => {
-    return (
-        <>
-            <div className='form'>
-                <TextMain classNameC={'main'}
-                    classNameP={'main__par'}
-                    textFirst={'Контакты'}
+  return (
+    <>
+      <section className="form-mob">
+        <div className="form">
+          <TextMain
+            classNameC={"main"}
+            classNameP={"main__par"}
+            textFirst={"Контакты"}
+          />
+          <nav className="form__main">
+            Свяжитесь с <span className="form__main-span">нами</span>
+          </nav>
+          <p className="form__sec">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna{" "}
+          </p>
+          <input type="text" placeholder="Ваше имя" className="form__input" />
+          <hr className="form__separator" />
+          <input
+            type="text"
+            placeholder="Ваш телефон"
+            className="form__input"
+          />
+          <hr className="form__separator" />
+          <input
+            type="text"
+            placeholder="VIN автомобиля"
+            className="form__input"
+          />
+          <hr className="form__separator" />
+          <Button className={"сontacts-btn"} text={"Заказать звонок"} />
+        </div>
+      </section>
+      <section className="form_pc">
+        <div className="form-pc">
+          <p className="form-pc__first">Контакты</p>
+          <nav className="form-pc__main">
+            Свяжитесь с <span className="form-pc__main-span">нами</span>
+          </nav>
+          <p className="form-pc__sec">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna{" "}
+          </p>
+          <div className="form-pc__inputs">
+            <div className="form-pc__inputs__f">
+              <div>
+                <input
+                  type="text"
+                  placeholder="Ваше имя"
+                  className="form-pc__inputs__input"
                 />
-                <nav className='form__main'>
-                    Свяжитесь с <span className='form__main-span'>нами</span>
-                </nav>
-                <p className='form__sec'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna </p>
-                <input type="text" placeholder='Ваше имя' className='form__input' />
-                <hr className='form__separator' />
-                <input type="text" placeholder='Ваш телефон' className='form__input' />
-                <hr className='form__separator' />
-                <input type="text" placeholder='VIN автомобиля' className='form__input' />
-                <hr className='form__separator' />
-                <Button className={"сontacts-btn"} text={'Заказать звонок'} />
+                <hr className="form-pc__inputs__separator" />
+              </div>
+              <div>
+                <input
+                  type="text"
+                  placeholder="Ваш телефон"
+                  className="form-pc__inputs__input"
+                />
+                <hr className="form-pc__inputs__separator" />
+              </div>
             </div>
-        </>
-    )
-}
+            <div>
+              <input
+                type="text"
+                placeholder="VIN автомобиля"
+                className="form-pc__inputs__input-s"
+              />
+              <hr className="form-pc__inputs__separator-s" />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Form
+export default Form;
--- src/Screens/ContactsScreen/ContactsScreen.jsx
+++ src/Screens/ContactsScreen/ContactsScreen.jsx
@@ -1,23 +1,45 @@
-import '../ContactsScreen/style.scss'
-import redCar from '../../assets/Group-437.webp'
+import "../ContactsScreen/style.scss";
+import redCar from "../../assets/Group-437.webp";
 
+import changan from "../../assets/changan-1.webp";
+import Form from "../../Components/Form/Form";
 
-import changan from '../../assets/changan-1.webp'
-import vector from '../../assets/Vector.webp'
-import zeekr from '../../assets/Vector-_1_.webp'
-import Form from '../../Components/Form/Form'
+import cPC from "../../assets/cPc.webp";
+import wol from "../../assets/wol.webp";
+import zeekr from "../../assets/Zeekr.webp";
+import bmw from "../../assets/bmw.webp";
 
 const ContactsScreen = () => {
-    return (
-        <div className='contacts'>
-            <img src={redCar} alt="" className='contacts__img' />
-            <div className="contacts__con">
-
-                <img src={changan} alt="" className='contacts__car' />
-            </div>
-            <Form />
+  return (
+    <>
+      <section className="contacts-mob">
+        <div className="contacts">
+          <img src={redCar} alt="" className="contacts__img" />
+          <div className="contacts__con">
+            <img src={changan} alt="" className="contacts__car" />
+          </div>
+          <Form />
         </div>
-    )
-}
+      </section>
+      <section className="contacts-pc">
+        <div className="contacts__pc">
+          <nav className="contacts__pc__nav">
+            <img src={cPC} alt="img" className="contacts__pc__img" />
+          </nav>
+          <nav className="contacts__pc__nav">
+            <img src={wol} alt="img" className="contacts__pc__img" />
+          </nav>
+          <nav className="contacts__pc__nav">
+            <img src={zeekr} alt="img" className="contacts__pc__zeekr" />
+          </nav>
+          <nav className="contacts__pc__nav">
+            <img src={bmw} alt="img" className="contacts__pc__img" />
+          </nav>
+        </div>
+      </section>
+      <Form />
+    </>
+  );
+};
 
-export default ContactsScreen
+export default ContactsScreen;
